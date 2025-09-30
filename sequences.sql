e967ce909bbed898753dc559dd849ba862cb0103
AJABHIMANYU <63732138+AJABHIMANYU@users.noreply.github.com>
2025-09-30 16:27:30 +0530

    Create sequences.sql

---
+++ sequences.sql
@@ -0,0 +1,6 @@
+-- sequences.sql
+-- Creates sequences for auto-incrementing IDs
+
+CREATE SEQUENCE card_seq START WITH 100 INCREMENT BY 1;
+CREATE SEQUENCE customer_seq START WITH 1 INCREMENT BY 1;
+CREATE SEQUENCE employee_seq START WITH 200 INCREMENT BY 1;
